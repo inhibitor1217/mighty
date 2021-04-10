682fea1d56d5f6bd645ffdc513348459107a4ecd
dugi <dugi@channel.io>
2021-04-10 23:18:05 +0900

    sign auth cookies at GET /auth/google/redirect

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/lines-between-class-members': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-floating-promises': 'error',
     'class-methods-use-this': 'off',
     'import/prefer-default-export': 'off',
   },
--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import type { Response } from 'express';
+import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
 import { AuthenticatedRequest } from './entity/authenticated-request';
 import { GoogleOauthGuard } from './guard/google-oauth.guard';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
   @Get('google')
   @UseGuards(GoogleOauthGuard)
   async googleOauth() {
@@ -12,9 +16,12 @@ export class AuthController {
 
   @Get('google/redirect')
   @UseGuards(GoogleOauthGuard)
-  async googleOauthRedirect(@Req() req: AuthenticatedRequest) {
+  async googleOauthRedirect(
+    @Req() req: AuthenticatedRequest,
+    @Res({ passthrough: true }) res: Response
+  ) {
     const { user } = req;
-
+    await this.authService.signAuthCookies(user, res.cookie.bind(res));
     return user;
   }
 }
