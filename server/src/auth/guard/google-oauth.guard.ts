cbac3e74eb9dd80327c2c51f4f95792dfd49d79a
dugi <dugi@channel.io>
2021-06-06 20:05:15 +0900

    handle invalid google oauth token error

--- server/src/auth/guard/google-oauth.guard.ts
+++ server/src/auth/guard/google-oauth.guard.ts
@@ -1,6 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import _ from 'lodash';
 import { GOOGLE_OAUTH_STRATEGY_NAME } from '../const';
 
 @Injectable()
-export class GoogleOauthGuard extends AuthGuard(GOOGLE_OAUTH_STRATEGY_NAME) {}
+export class GoogleOauthGuard extends AuthGuard(GOOGLE_OAUTH_STRATEGY_NAME) {
+  handleRequest<TUser>(err?: any, user?: TUser): TUser {
+    if (!_.isNil(err) || _.isNil(user)) {
+      throw new UnauthorizedException(err);
+    }
+
+    return user;
+  }
+}
