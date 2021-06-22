abbe5347339aa8424cd28b8e0f29b26cb337d166
dugi <dugi@channel.io>
2021-06-22 23:18:10 +0900

    implemet UI for BannedUserInfo

---
+++ web/src/component/feature/User/BannedUserInfo/BannedUserInfo.styled.ts
@@ -0,0 +1,22 @@
+import { styled } from "@channel.io/bezier-react";
+
+import flexbox from "styles/flexbox";
+
+const TitleWrapper = styled.div`
+  ${flexbox("row")}
+
+  gap: 8px;
+`;
+
+const Wrapper = styled.div`
+  ${flexbox("column", "center", "flex-start")}
+
+  padding-right: 24px;
+`;
+
+const Styled = {
+  TitleWrapper,
+  Wrapper,
+};
+
+export default Styled;
